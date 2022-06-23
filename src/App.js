551522e57f7a9059242e4e8c2ae8053ab32f179a
nvquinter <valuquintero@gmail.com>
2022-06-23 17:48:52 -0300

    eventos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Nav from "./components/NavBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import Carrito from "./components/Carrito";
+import Checkout from "./components/Checkout";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
          <Route path="/" element= {<ItemListContainer />} />
          <Route path="/detail/:id" element= {<ItemDetailContainer />} />
          <Route path="/category/:categoryId" element= {<ItemListContainer />} />
+         <Route path="/carrito/" element= {<Carrito />} />
+         <Route path="/checkout/" element= {<Checkout />} />
        </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Carrito.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Carrito = () => {
+  return (
+    <div>
+        <h1>Carrito</h1></div>
+  )
+}
+
+export default Carrito
